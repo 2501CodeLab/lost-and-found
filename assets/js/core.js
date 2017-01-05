aa97697470c92972e0c0cbc84e49a11110ab4a17
Zyplos <officialzyplos@gmail.com>
2017-01-05 16:33:54 -0600

    Front Page
    Post Loading and Managing functions

--- assets/js/core.js
+++ assets/js/core.js
@@ -6,4 +6,15 @@ var config = {
     messagingSenderId: "371485198623"
   };
   firebase.initializeApp(config);
+
+console.log('%c --------------------------------------', 'color: #ff3e3e;');
+console.log('%c rude', 'color: #ff3e3e;');
+console.log('%c ', 'color: #ff3e3e;');
+console.log('%c snooping around the console like that', 'color: #ff3e3e;');
+console.log('%c Just kidding.', 'color: #ff3e3e;');
+console.log('%c That\'s okay, if anything, we encourage it! Check out our Github at: https://github.com/2501CodeLab/', 'color: #ff3e3e;');
+console.log('%c All of our projects\' code is usually there.', 'color: #ff3e3e;');
+console.log('%c Found a bug? Broke something? Got an error? Report it to us!', 'color: #ff3e3e;');
+console.log('%c --------------------------------------', 'color: #ff3e3e;');
+
 console.log('%c>> CORE LOADED.', 'color: #ff3e3e;');
